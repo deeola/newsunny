3e09ced8eb889db909b65f8c379c503c544e36aa
Adeola Bamigboye <clazikar@gmail.com>
2021-06-30 00:24:33 +0200

    manifest and icons added

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -1,7 +1,6 @@
 // CALL INSTALL EVENT
 
-
-const myCached = 'd1';
+const myCached = "d1";
 
 // const myCatchedAssests = [
 //     '/',
@@ -16,77 +15,63 @@ const myCached = 'd1';
 
 // ]
 
+self.addEventListener("install", (e) => {
+  console.log("service worker installed");
 
-self.addEventListener('install', (e) => {
-
-    console.log('service worker installed')
-
-    // e.waitUntil(
-    //     caches.open(myCached)
-    //         .then( cache => {
-    //             console.log('sevice worker: catching files')
-    //             cache.addAll(myCatchedAssests)
-    //         })
-    //         .then(() => self.skipWaiting())
-    // )
-
-})
+  // e.waitUntil(
+  //     caches.open(myCached)
+  //         .then( cache => {
+  //             console.log('sevice worker: catching files')
+  //             cache.addAll(myCatchedAssests)
+  //         })
+  //         .then(() => self.skipWaiting())
+  // )
+});
 
 //CALL THE ACTIVATE EVENT
 
-self.addEventListener('activate', (e) => {
+self.addEventListener("activate", (e) => {
+  console.log("service worker activated");
 
-    console.log('service worker activated')
+  // Remove unwanted caches
 
-    // Remove unwanted caches
-
-    e.waitUntil(
-        caches.keys().then(
-            cachNames  => {
-                return Promise.all(
-                cachNames.map(cache => {
-
-                        if(cache !== myCached){
-                            return caches.delete(cache) 
-                        }
-                        
-                    })
-                )
-            }
-        )
-    )
-
-})
+  e.waitUntil(
+    caches.keys().then((cachNames) => {
+      return Promise.all(
+        cachNames.map((cache) => {
+          if (cache !== myCached) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
 
 //CALL FETCH EVENT
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener("fetch", (e) => {
+  console.log("service worker fecthing");
+  if (!(e.request.url.indexOf("http") === 0)) return;
 
-    console.log('service worker fecthing')
-    if (!(e.request.url.indexOf('http') === 0)) return;
+  e.respondWith(
+    // fetch(e.request).catch( () => caches.match(e.request))
 
-    e.respondWith(
-        // fetch(e.request).catch( () => caches.match(e.request))
+    fetch(e.request)
+      .then((res) => {
+        //make copy/clone
+        const resClone = res.clone();
 
-        fetch(e.request)
-            .then(res => {
-                //make copy/clone
-                const resClone = res.clone()
+        //open cache
 
-                //open cache
+        caches.open(myCached).then((cache) => {
+          //Add response to cache
 
-                caches.open(myCached)
-                    .then(cache => {
+          cache.put(e.request, resClone);
+        });
 
-                        //Add response to cache
-
-                        cache.put(e.request, resClone)
-
-                    });
-
-                    return res
-            }).catch(
-                err => caches.match(e.request).then(res => res)
-            )
-    )
-})
+        return res;
+      })
+      .catch((err) => caches.match(e.request).then((res) => res))
+  );
+});
