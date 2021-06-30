018b641a840700dbc004af6a4d46ca3093bc09ef
Adeola Bamigboye <clazikar@gmail.com>
2021-06-30 19:56:03 +0200

    sw

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ if('serviceWorker' in  navigator){
     window.addEventListener('load', () => {
         navigator.serviceWorker
         .register('/sw_cached_pages.js')
-        .then(reg => console.log('service worker registered'))
+        .then(reg => console.log('service worker registered', reg))
         .catch(err => console.log(`service worker : error : ${err}`))
     })
 }
